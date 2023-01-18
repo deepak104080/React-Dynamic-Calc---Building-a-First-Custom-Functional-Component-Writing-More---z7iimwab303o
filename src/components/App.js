d4c9b2995415c3c74eff52ae3c9a5b12a63bf875
deepak104080 <deepak104080@gmail.com>
2023-01-18 23:03:11 +0530

    assg

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,28 @@
 import React,{useState,useEffect} from 'react'
 import '../styles/App.css';
 const App = () => {
+  const [input1, setInput1] = useState(0);
+  const [input2, setInput2] = useState(0);
+
+  const tempFn1 = (e) => {
+    console.log('tempFn', e.target.value, typeof(e.target.value));
+    let inputNum = parseInt(e.target.value);
+    setInput1(inputNum);
+  }
+
+  const tempFn2 = (e) => {
+    console.log('tempFn', e.target.value, typeof(e.target.value));
+    let inputNum = parseInt(e.target.value);
+    setInput2(inputNum);
+  }
 
   return (
     <div id="main">
-      <input id='input1' onChange={}/>
+      <input id='input1' onChange={tempFn1}/>
        +
-      <input id='input2' onChange={}/> 
+      <input id='input2' onChange={tempFn2}/>
       
-      <p id='result'></p>
+      <p id='result'>{input1 + input2}</p>
     </div>
   )
 }
